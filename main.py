db5b5c7e69d4725895e628f6c6c8c34056c160f1
epee33 <ephenry44@gmail.com>
2025-11-07 13:26:30 -0500

    Implemented PRF & added dependencies

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("testing")
+import os
+from Crypto.Cipher import AES 
+
+
+def prf(key, input): 
+    if len(key) != 16 or len(input) != 16: #key and input has to be 16 bytes for AES-128
+        raise ValueError("Key and input must be 16 bytes long")
+    
+    cipher = AES.new(key, AES.MODE_ECB) #create AES cipher in ECB mode
+    output = cipher.encrypt(input) #encrypt the input 
+    return output
