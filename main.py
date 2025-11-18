c0f26f5c28f44f00f393c5ca65d758c4ba05fcdf
epee33 <ephenry44@gmail.com>
2025-11-18 14:59:39 -0600

    added mix and match attack

--- main.py
+++ main.py
@@ -11,10 +11,10 @@ def prf(key, input):
     return output
 
 def pad_to_16(data): #pad data to 16 bytes
-    needed = 16 - len(data) 
-    if needed > 0: 
+    needed = 16 - len(data)
+    if needed > 0:
         data = data + (b'\x00' * needed)
-    return data
+    return data[:16] #ensure data is exactly 16 bytes
 
 def split_blocks(message, size): #very basic block splitting function   
     blocks = []
@@ -41,5 +41,33 @@ def mac_compute(key, message, block_size=15):
 def mac_verify(key, message, tag): #If the computed tag matches the provided tag then autentication is successful
     expected = mac_compute(key, message)
     return expected == tag
+
+if __name__ == "__main__": #mix and match attack 
+    key = os.urandom(16)
+
+    message1 = b"I love cryptography"
+    tag1 = mac_compute(key, message1)
+
+    message2 = b"CS549 is great!!!"
+    tag2 = mac_compute(key, message2)
+
+    print("Message 1:", message1)
+    print("Tag 1:", tag1.hex())
+    print("Message 2:", message2)
+    print("Tag 2:", tag2.hex())
+
+    M1_blocks = split_blocks(message1, 15)
+    M2_blocks = split_blocks(message2, 15)
+    
+    forged_message = M1_blocks[0] + M2_blocks[1] #mix and match attack 
+    forged_tag = tag1[:16] + tag2[16:]
+
+    print("Forged Message:", forged_message)
+    print("Forged Tag:", forged_tag.hex())
+    if mac_verify(key, forged_message, forged_tag):
+        print("Forgery successful! The forged message is authenticated.")
+    else:
+        print("Forgery failed! The forged message is not authenticated.")
+
 
 
